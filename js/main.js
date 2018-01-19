056210741b883f57bcf75e6cfc30d930596eecc1
Riccardo Schifaudo <riccardo.schifaudo@inrebus.it>
2018-01-19 16:04:42 +0100

    sezione about e interests

--- js/main.js
+++ js/main.js
@@ -3,4 +3,11 @@ $(document).ready(function(){
 		$(".schiri_sub_header").slideToggle();
 		$(".schiri_menu img").toggleClass("schiri_menu_active");
 	});
+	$('a[href^="#"]').click(function () {
+		$('html, body').animate({
+			scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top-100
+		}, 500);
+
+		return false;
+	});
 });
