1f595088198c5017b8888766ca5db178d34eb71d
Riccardo Schifaudo <riccardo.schifaudo@inrebus.it>
2018-01-19 17:35:03 +0100

    fix js main e sub menu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 		$(".schiri_sub_header").slideToggle();
 		$(".schiri_menu img").toggleClass("schiri_menu_active");
 	});
+	$(".schiri_sub_header li a").click(function(){
+		$(".schiri_sub_header").slideToggle();
+	});
 	$('a[href^="#"]').click(function () {
 		$('html, body').animate({
 			scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top-100
