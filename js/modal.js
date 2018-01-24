c8941ea844767886ff11cc299528ba1b3e7b947e
Riccardo Schifaudo <riccardo.schifaudo@inrebus.it>
2018-01-24 15:13:10 +0100

    disable overflow open modal

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,8 @@
 $(".schiri_btn_modal_cv").click(function(){
+	$("body").addClass("disable_overflow");
 	$(".schiri_modal").fadeIn();
 });
 $(".schiri_btn_modal_close").click(function(){
+	$("body").removeClass("disable_overflow");
 	$(".schiri_modal").fadeOut();
 });
